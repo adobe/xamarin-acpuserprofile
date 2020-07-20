42b6f5829e4db8e458480b61e90f967d2367612f
Ryan Morales <rymorale@adobe.com>
2020-07-20 14:57:12 -0700

    fix iOS binding assembly info and bump iOS version to 0.0.2

--- src/Adobe.ACPUserProfile.iOS/Properties/AssemblyInfo.cs
+++ src/Adobe.ACPUserProfile.iOS/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using Foundation;
 [assembly: AssemblyDescription("ACPUserProfile for Xamarin iOS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Adobe Inc.")]
-[assembly: AssemblyProduct("ACPAnalytics")]
+[assembly: AssemblyProduct("ACPUserProfile")]
 [assembly: AssemblyCopyright("Copyright 2020 Adobe. All rights reserved. This file is licensed to you under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -37,8 +37,8 @@ using Foundation;
 //      Major Version
 //      Minor Version 
 //      Patch Version
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
